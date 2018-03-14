83f75a95dbb569fc1abd57aeed665e825fdd0e16
Eldon Hipolito <eldon.hipolito@pointwest.com.ph>
2018-03-14 14:49:46 +0800

    Identity contract

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -1,12 +1,30 @@
 pragma solidity 0.4.19;
 
+import 'zeppelin-solidity/contracts/ECRecovery.sol';
+
 contract Identity {
 
     string public name;
 
-    address
+    address public owner;
 
-    function Identity(){
 
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
     }
+
+
+    function Identity(string _name) public {
+        name = _name;
+        owner = msg.sender;
+    }
+
+    // Acts as the sign-in function for the dapp
+    function authenticate(bytes32 hash, bytes sig) public constant onlyOwner returns (bool) {
+        return ECRecovery.recover(hash, sig) == msg.sender;
+    }
+
+
+
 }
