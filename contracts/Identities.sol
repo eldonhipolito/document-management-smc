7e6bd276bd35f27b110178005b6decab2e7d3b45
Eldon Hipolito <eldon.hipolito@pointwest.com.ph>
2018-04-03 16:06:02 +0800

    Added unit tests

--- contracts/Identities.sol
+++ contracts/Identities.sol
@@ -2,9 +2,9 @@ pragma solidity ^0.4.18;
 
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
-import './RBACIntf.sol';
+import 'zeppelin-solidity/contracts/ownership/rbac/RBAC.sol';
 
-contract Identities is Ownable {
+contract Identities is Ownable, RBAC {
 
     struct VerificationData {
         address user;
@@ -29,16 +29,13 @@ contract Identities is Ownable {
 
     string constant ROLE_DOCUMENT_CREATOR = "creator";
 
-    string constant ROLE_ADMIN = "admin";
-
     bytes32 constant ROLE_ADMIN_KCK = 0xf23ec0bb4210edd5cba85afd05127efcd2fc6a781bfed49188da1081670b22d8;
 
-    RBACIntf public rbacAddress;
 
     bytes32 constant IDENTITY = 0x377799b22fba826cf24c3f07e6731c67676765addcee33415a2c80d453d4ed6e;
 
-    function Identities(address _rbacAddress) public {
-        rbacAddress = RBACIntf(_rbacAddress);
+    function Identities() public {
+
     }
 
     event IdentityVerified(address verifier, address user, address identity);
@@ -46,15 +43,9 @@ contract Identities is Ownable {
     event RoleAdded(address admin, address user, string role);
     event RoleRemoved(address admin, address user, string role);
 
-    modifier onlyRole(string _role) {
-        checkRole(msg.sender, _role);
-        _;
-    }
 
-    modifier onlyAdmin() {
-        checkRole(msg.sender, ROLE_ADMIN);
-        _;
-    }
+
+
 
     function verifyIdentity(address user, address identity) public onlyAdmin {
         require(IdentityOwnership(identity).owner() == user);
@@ -78,10 +69,6 @@ contract Identities is Ownable {
     }
 
 
-    function setRBACAddress(address _rbacAddress) public onlyOwner {
-        rbacAddress = RBACIntf(_rbacAddress);
-    }
-
     function checkVerifiedRole(address addr) view public {
         checkRole(addr, ROLE_VERIFIED_IDENTITY);
     }
@@ -92,31 +79,6 @@ contract Identities is Ownable {
 
     function checkCreatorRole(address addr) view public {
         checkRole(addr, ROLE_DOCUMENT_CREATOR);
-    }
-
-
-    function hasRole(address addr, string roleName) public view returns (bool) {
-        return rbacAddress.hasRole(addr, roleName);
-    }
-
-    function checkRole(address addr, string roleName) public view {
-        rbacAddress.checkRole(addr, roleName);
-    }
-
-    function adminAddRole(address addr, string roleName) public onlyAdmin {
-        require(identities[addr] != address(0));
-
-        rbacAddress.adminAddRole(addr, roleName);
-
-        RoleAdded(msg.sender, addr, roleName);
-
-    }
-
-    function adminRemoveRole(address addr, string roleName) public onlyAdmin {
-        require(ROLE_VERIFIED_IDENTITY_KCK != keccak256(roleName));
-        rbacAddress.adminRemoveRole(addr, roleName);
-
-        RoleRemoved(msg.sender, addr, roleName);
     }
 
 
